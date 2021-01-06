749e9573f95a03165e076faa453d95a81f6a2f88
Victor <victor@grycuk.net>
2021-01-06 10:23:55 -0300

    Add TranslationResult class

---
+++ samples/17.multilingual-bot/src/main/java/com/microsoft/bot/sample/multilingual/translation/model/TranslationResult.java
@@ -0,0 +1,46 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.microsoft.bot.sample.multilingual.translation.model;
+
+/**
+ * Translation result from Translator API v3.
+ */
+public class TranslationResult {
+    private String text;
+
+    private String to;
+
+
+    /**
+     * Gets the translation result text.
+     * @return Translation result.
+     */
+    public String getText() {
+        return this.text;
+    }
+
+    /**
+     * Sets the translation result text.
+     * @param resultText Translation result.
+     */
+    public void setText(String resultText) {
+        this.text = resultText;
+    }
+
+    /**
+     * Gets the target language locale.
+     * @return Locale.
+     */
+    public String getTo() {
+        return this.to;
+    }
+
+    /**
+     * Sets the target language locale.
+     * @param locale Target locale.
+     */
+    public void setTo(String locale) {
+        this.to = locale;
+    }
+}

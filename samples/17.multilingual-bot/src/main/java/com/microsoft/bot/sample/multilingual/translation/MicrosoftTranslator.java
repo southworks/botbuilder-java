47d861491f818ce201aa17e3de8014138993c4ed
Victor <victor@grycuk.net>
2021-01-07 10:48:12 -0300

    Finish migration

--- samples/17.multilingual-bot/src/main/java/com/microsoft/bot/sample/multilingual/translation/MicrosoftTranslator.java
+++ samples/17.multilingual-bot/src/main/java/com/microsoft/bot/sample/multilingual/translation/MicrosoftTranslator.java
@@ -3,73 +3,89 @@
 
 package com.microsoft.bot.sample.multilingual.translation;
 
+import java.io.Reader;
+import java.io.StringReader;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microsoft.bot.integration.Configuration;
+import com.microsoft.bot.sample.multilingual.translation.model.TranslatorResponse;
 
 import org.slf4j.LoggerFactory;
 
 import okhttp3.MediaType;
-import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
+/**
+ * A helper class wrapper for the Microsoft Translator API.
+ */
 public class MicrosoftTranslator {
     private static final String HOST = "https://api.cognitive.microsofttranslator.com";
     private static final String PATH = "/translate?api-version=3.0";
     private static final String URI_PARAMS = "&to=";
 
     private static OkHttpClient httpClient = new OkHttpClient();
-
     private static String key;
 
+    /**
+     * @param configuration The configuration class with the translator key stored.
+     */
     public MicrosoftTranslator(Configuration configuration) {
-        String key = configuration.getProperty("TranslatorKey");
+        String translatorKey = configuration.getProperty("TranslatorKey");
 
-        if (key == null) {
+        if (translatorKey == null) {
             throw new IllegalArgumentException("key");
         }
 
-        MicrosoftTranslator.key = key;
+        MicrosoftTranslator.key = translatorKey;
     }
 
-    public CompletableFuture<String> Translate(String text, String targetLocale) {
-        // From Cognitive Services translation documentation:
-        // https://docs.microsoft.com/en-us/azure/cognitive-services/Translator/quickstart-translator?tabs=java
-        String json = String.format("[{ \"Text\": \"%s\" }]", text);
+    /**
+     * Helper method to translate text to a specified language.
+     * @param text Text Text that will be translated.
+     * @param targetLocale targetLocale Two character language code, e.g. "en", "es".
+     * @return The first translation result
+     */
+    public CompletableFuture<String> translate(String text, String targetLocale) {
+        return CompletableFuture.supplyAsync(() -> {
+            // From Cognitive Services translation documentation:
+            // https://docs.microsoft.com/en-us/azure/cognitive-services/Translator/quickstart-translator?tabs=java
+            String json = String.format("[{ \"Text\": \"%s\" }]", text);
 
-        String uri =
-            MicrosoftTranslator.HOST + MicrosoftTranslator.PATH + MicrosoftTranslator.URI_PARAMS + targetLocale;
+            String uri =
+                MicrosoftTranslator.HOST + MicrosoftTranslator.PATH + MicrosoftTranslator.URI_PARAMS + targetLocale;
 
-        RequestBody requestBody = RequestBody.create(MediaType.parse("application/json"), json);
+            RequestBody requestBody = RequestBody.create(MediaType.parse("application/json"), json);
 
-        Request request = new Request.Builder()
-            .url(uri)
-            .header("Ocp-Apim-Subscription-Key", MicrosoftTranslator.key)
-            .post(requestBody)
-            .build();
+            Request request = new Request.Builder()
+                .url(uri)
+                .header("Ocp-Apim-Subscription-Key", MicrosoftTranslator.key)
+                .post(requestBody)
+                .build();
 
-        OkHttpClient client = new OkHttpClient();
+            try {
+                Response response = MicrosoftTranslator.httpClient.newCall(request).execute();
 
-        try {
-            Response response = client.newCall(request).execute();
+                if (!response.isSuccessful()) {
+                    String message =  String.format("The call to the translation service returned HTTP status code %s",
+                                        response.code() + ".");
+                    throw new Exception(message);
+                }
 
-            if (!response.isSuccessful()) {
-                String message = "The call to the translation service returned HTTP status code " + response.code();
-                throw new Exception(message);
-            }
+                ObjectMapper objectMapper = new ObjectMapper();
+                Reader reader = new StringReader(response.body().string());
+                TranslatorResponse[] result = objectMapper.readValue(reader, TranslatorResponse[].class);
 
-            ObjectMapper objectMapper = new ObjectMapper();
-            TranslatorResponse[] car = objectMapper.readValue(response.body(), TranslatorResponse.class);
+                return result[0].getTranslations().get(0).getText();
 
-        } catch (Exception e) {
-            LoggerFactory.getLogger(MicrosoftTranslator.class)
-                    .error("findPackages", e);
+            } catch (Exception e) {
+                LoggerFactory.getLogger(MicrosoftTranslator.class).error("findPackages", e);
                 throw new CompletionException(e);
-        }
+            }
+        });
     }
 }

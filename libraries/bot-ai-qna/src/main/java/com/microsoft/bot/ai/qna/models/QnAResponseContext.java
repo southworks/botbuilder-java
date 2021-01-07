b6eb6e13d6083d45e36dcf3e376a455fc761d93d
Martin Battaglino <martinbatta32@gmail.com>
2021-01-07 17:16:07 -0300

    Add QnARequestContext, QnAResponseContext and QueryResult files
    * Add migrated files
    
    * Replace Arraylist to array
    
    * Add period

--- libraries/bot-ai-qna/src/main/java/com/microsoft/bot/ai/qna/models/QnAResponseContext.java
+++ libraries/bot-ai-qna/src/main/java/com/microsoft/bot/ai/qna/models/QnAResponseContext.java
@@ -2,3 +2,30 @@
 // Licensed under the MIT License.
 
 package com.microsoft.bot.ai.qna.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * The context associated with QnA.  Used to mark if the qna response has related prompts to display.
+ */
+public class QnAResponseContext {
+
+    @JsonProperty("prompts")
+    private QnAMakerPrompt[] prompts;
+
+    /**
+     * Gets the prompts collection of related prompts.
+     * @return The QnA prompts array.
+     */
+    public QnAMakerPrompt[] getPrompts() {
+        return this.prompts;
+    }
+
+    /**
+     * Sets the prompts collection of related prompts.
+     * @param withPrompts The QnA prompts array.
+     */
+    public void setPrompts(QnAMakerPrompt[] withPrompts) {
+        this.prompts = withPrompts;
+    }
+}

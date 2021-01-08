13f7e42090283a691b2fe2182e4aeb6525322ebe
Martin Battaglino <martinbatta32@gmail.com>
2021-01-08 15:07:43 -0300

    Fix static props naming

--- libraries/bot-ai-qna/src/main/java/com/microsoft/bot/ai/qna/models/RankerTypes.java
+++ libraries/bot-ai-qna/src/main/java/com/microsoft/bot/ai/qna/models/RankerTypes.java
@@ -11,15 +11,15 @@ public class RankerTypes {
     /**
      * Default Ranker Behaviour. i.e. Ranking based on Questions and Answer.
      */
-    public static final String DEFAULTRANKERTYPE = "Default";
+    public static final String DEFAULT_RANKER_TYPE = "Default";
 
     /**
      * Ranker based on question Only.
      */
-    public static final String QUESTIONONLY = "QuestionOnly";
+    public static final String QUESTION_ONLY = "QuestionOnly";
 
     /**
      * Ranker based on Autosuggest for question field Only.
      */
-    public static final String AUTOSUGGESTQUESTION = "AutoSuggestQuestion";
+    public static final String AUTO_SUGGEST_QUESTION = "AutoSuggestQuestion";
 }

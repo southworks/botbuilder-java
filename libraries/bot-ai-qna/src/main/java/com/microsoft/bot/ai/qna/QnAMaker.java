d72e6e11f3fa86b9b2d7dfe5b38c934133b16e2c
Franco Alvarez <franco.alvarez@southworks.com>
2021-01-14 15:06:12 -0300

    Add license and copyright

--- libraries/bot-ai-qna/src/main/java/com/microsoft/bot/ai/qna/QnAMaker.java
+++ libraries/bot-ai-qna/src/main/java/com/microsoft/bot/ai/qna/QnAMaker.java
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.microsoft.bot.ai.qna;
 
 import com.microsoft.bot.ai.qna.models.FeedbackRecords;

992344d2df596c3c3be7bdf73d0514a1b935034d
Victor <victor@grycuk.net>
2021-02-25 11:29:48 -0300

    Fix null values

--- libraries/bot-azure/src/main/java/com/microsoft/bot/azure/CosmosDbPartitionedStorageOptions.java
+++ libraries/bot-azure/src/main/java/com/microsoft/bot/azure/CosmosDbPartitionedStorageOptions.java
@@ -31,6 +31,7 @@ public class CosmosDbPartitionedStorageOptions {
         connectionPolicy = ConnectionPolicy.GetDefault();
         consistencyLevel = DEFAULT_CONSISTENCY;
         containerThroughput = DEFAULT_THROUGHPUT;
+        compatibilityMode = true;
     }
 
     /**

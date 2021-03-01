2cc62a06c66cfa580ca20292e1d47755a650e10a
Martin Battaglino <martinbatta32@gmail.com>
2021-03-01 12:32:06 -0300

    Add missing ActivityEventNames

---
+++ libraries/bot-schema/src/main/java/com/microsoft/bot/schema/ActivityEventNames.java
@@ -0,0 +1,19 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.microsoft.bot.schema;
+
+/**
+ *  Define values for common event names used by activities of type {@link ActivityTypes.EVENT}
+ */
+public class ActivityEventNames {
+    /**
+     * The event name for continuing a conversation.
+     */
+    public static final String CONTINUE_CONVERSATION = "ContinueConversation";
+
+    /**
+     * The event name for creating a conversation.
+     */
+    public static final String CREATE_CONVERSATION = "CreateConversation";
+}

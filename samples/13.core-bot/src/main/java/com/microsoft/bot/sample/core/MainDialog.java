c40fa0622c3306a4ea4843de4d838450a5e53111
Martin Battaglino <martinbatta32@gmail.com>
2021-03-02 19:52:44 -0300

    Update MainDialog and fix issues

--- samples/13.core-bot/src/main/java/com/microsoft/bot/sample/core/MainDialog.java
+++ samples/13.core-bot/src/main/java/com/microsoft/bot/sample/core/MainDialog.java
@@ -17,7 +17,9 @@ import com.microsoft.bot.schema.InputHints;
 import com.microsoft.recognizers.datatypes.timex.expression.TimexProperty;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
@@ -25,100 +27,126 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.commons.lang3.StringUtils;
 
 public class MainDialog extends ComponentDialog {
-    private FlightBookingRecognizer luisRecognizer;
-    private String MAIN_WATERFALL_DIALOG = "mainWaterfallDialog";
+    private final FlightBookingRecognizer luisRecognizer;
 
     public MainDialog(FlightBookingRecognizer withLuisRecognizer, BookingDialog bookingDialog) {
-        super("MainDialog");
+        super(MainDialog.class.getName());
 
         luisRecognizer = withLuisRecognizer;
 
-        addDialog(new TextPrompt("TextPrompt"));
+        addDialog(new TextPrompt(TextPrompt.class.getName()));
         addDialog(bookingDialog);
-        addDialog(new WaterfallDialog(MAIN_WATERFALL_DIALOG, new ArrayList<WaterfallStep>() {
-            {
-                add(introStep);
-                add(actStep);
-                add(finalStep);
-            }
-        }));
+        WaterfallStep[] waterfallSteps = {
+            this::introStep,
+            this::actStep,
+            this::finalStep
+        };
+        addDialog(new WaterfallDialog(WaterfallDialog.class.getName(), Arrays.asList(waterfallSteps)));
 
-        setInitialDialogId(MAIN_WATERFALL_DIALOG);
+        // The initial child Dialog to run.
+        setInitialDialogId(WaterfallDialog.class.getName());
     }
 
+    /**
+     * First step in the waterfall dialog. Prompts the user for a command.
+     * Currently, this expects a booking request, like "book me a flight from Paris to Berlin on march 22"
+     * Note that the sample LUIS model will only recognize Paris, Berlin, New York and London as airport cities.
+     * @param stepContext A {@link WaterfallStepContext}
+     * @return A {@link DialogTurnResult}
+     */
     private CompletableFuture<DialogTurnResult> introStep(WaterfallStepContext stepContext) {
-        if (!luisRecognizer.isConfigured) {
-            stepContext.getContext().sendActivity
-                (MessageFactory.text
-                    ("NOTE: LUIS is not configured. To enable all capabilities, add 'LuisAppId', 'LuisAPIKey' and 'LuisAPIHostName' to the appsettings.json file.", InputHints.IGNORING_INPUT))
-                .thenCompose(sendResult -> stepContext.next(null).thenApply(nextResult -> null));
+        if (!luisRecognizer.isConfigured()) {
+            Activity text = MessageFactory.text("NOTE: LUIS is not configured. To enable all capabilities, add 'LuisAppId', 'LuisAPIKey' and 'LuisAPIHostName' to the appsettings.json file.", null, InputHints.IGNORING_INPUT);
+            return stepContext.getContext().sendActivity(text)
+                .thenCompose(sendResult -> stepContext.next(null));
         }
 
         // Use the text provided in FinalStepAsync or the default if it is the first time.
-        String weekLaterDate = LocalDateTime.now().plusDays(7).toString();
-        String messageText = (stepContext.getOptions() != null)? stepContext.getOptions().toString() : String.format("What can I help you with today?\nSay something like \"Book a flight from Paris to Berlin on %s\"", weekLaterDate);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM d, yyyy");
+        String weekLaterDate = LocalDateTime.now().plusDays(7).format(formatter);
+        String messageText = stepContext.getOptions() != null ? stepContext.getOptions().toString() : String.format("What can I help you with today?\nSay something like \"Book a flight from Paris to Berlin on %s\"", weekLaterDate);
         Activity promptMessage = MessageFactory.text(messageText, messageText, InputHints.EXPECTING_INPUT);
-        return stepContext.prompt("TextPrompt", new PromptOptions() {{ setPrompt(promptMessage); }});
+        return stepContext.prompt(TextPrompt.class.getName(), new PromptOptions(){
+                {
+                    setPrompt(promptMessage);
+                }
+        });
     }
 
+    /**
+     * Second step in the waterfall.  This will use LUIS to attempt to extract the origin, destination and travel dates.
+     * Then, it hands off to the bookingDialog child dialog to collect any remaining details.
+     * @param stepContext A {@link WaterfallStepContext}
+     * @return A {@link DialogTurnResult}
+     */
     private CompletableFuture<DialogTurnResult> actStep(WaterfallStepContext stepContext) {
-        if (!luisRecognizer.isConfigured) {
+        if (!luisRecognizer.isConfigured()) {
             // LUIS is not configured, we just run the BookingDialog path with an empty BookingDetailsInstance.
-            return stepContext.beginDialog("BookingDialog", new BookingDetails());
+            return stepContext.beginDialog(BookingDialog.class.getName(), new BookingDetails());
         }
 
         // Call LUIS and gather any potential booking details. (Note the TurnContext has the response to the prompt.)
-        luisRecognizer.recognize<FlightBooking>(stepContext.getContext()).thenCompose(luisResult -> {
-                switch (luisResult.topIntent().intent) {
-                    case "BookFlight":
-                        ObjectNode fromEntities = this.luisRecognizer.getFromEntities(luisResult);
-                        ObjectNode toEntities = this.luisRecognizer.getToEntities(luisResult);
+        return luisRecognizer.recognize(stepContext.getContext()).thenCompose(luisResult -> {
+            switch (luisResult.getTopScoringIntent().intent) {
+                case "BookFlight":
+                    // Extract the values for the composite entities from the LUIS result.
+                    ObjectNode fromEntities = luisRecognizer.getFromEntities(luisResult);
+                    ObjectNode toEntities = luisRecognizer.getToEntities(luisResult);
 
-                        showWarningForUnsupportedCities(stepContext.getContext(), fromEntities, toEntities).thenApply( showResult -> {
-                                // Initialize BookingDetails with any entities we may have found in the response.
-                                BookingDetails bookingDetails = new BookingDetails() {
-                                    {
-                                        // Get destination and origin from the composite entities arrays.
-                                        setDestination(luisResult.getToEntities().getAirport());
-                                        setOrigin(luisResult.getFromEntities().getAirport());
-                                        setTravelDate(luisResult.getTravelDate());
-                                    }
-                                };
-                                // Run the BookingDialog giving it whatever details we have from the LUIS call, it will fill out the remainder.
-                                return stepContext.beginDialog("BookingDialog", bookingDetails);
-                            }
-                        );
-                        break;
-                    case "GetWeather":
-                        // We haven't implemented the GetWeatherDialog so we just display a TODO message.
-                        String getWeatherMessageText = "TODO: get weather flow here";
-                        Activity getWeatherMessage = MessageFactory.text(getWeatherMessageText, getWeatherMessageText, InputHints.IGNORING_INPUT);
-                        return stepContext.getContext().sendActivity(getWeatherMessage);
-                        break;
-                    default:
-                        // Catch all for unhandled intents
-                        String didntUnderstandMessageText = String.format("Sorry, I didn't get that. Please try asking in a different way (intent was %s)", luisResult.topIntent()intent);
-                        Activity didntUnderstandMessage = MessageFactory.text(didntUnderstandMessageText, didntUnderstandMessageText, InputHints.IGNORING_INPUT);
-                        stepContext.getContext().sendActivity(didntUnderstandMessage);
-                        break;
-            }});
-
-        return stepContext.next(null);
+                    // Show a warning for Origin and Destination if we can't resolve them.
+                    return showWarningForUnsupportedCities(stepContext.getContext(), fromEntities, toEntities).thenCompose(showResult -> {
+                            // Initialize BookingDetails with any entities we may have found in the response.
+                            BookingDetails bookingDetails = new BookingDetails() {
+                                {
+                                    // Get destination and origin from the composite entities arrays.
+                                    setDestination(toEntities.get("airport").toString());
+                                    setOrigin(fromEntities.get("airport").toString());
+                                    setTravelDate(luisRecognizer.getTravelDate(luisResult));
+                                }
+                            };
+                            // Run the BookingDialog giving it whatever details we have from the LUIS call, it will fill out the remainder.
+                            return stepContext.beginDialog(BookingDialog.class.getName(), bookingDetails);
+                        }
+                    );
+                case "GetWeather":
+                    // We haven't implemented the GetWeatherDialog so we just display a TODO message.
+                    String getWeatherMessageText = "TODO: get weather flow here";
+                    Activity getWeatherMessage = MessageFactory.text(getWeatherMessageText, getWeatherMessageText, InputHints.IGNORING_INPUT);
+                    stepContext.getContext().sendActivity(getWeatherMessage);
+                    break;
+                default:
+                    // Catch all for unhandled intents
+                    String didntUnderstandMessageText = String.format("Sorry, I didn't get that. Please try asking in a different way (intent was %s)", luisResult.getTopScoringIntent().intent);
+                    Activity didntUnderstandMessage = MessageFactory.text(didntUnderstandMessageText, didntUnderstandMessageText, InputHints.IGNORING_INPUT);
+                    stepContext.getContext().sendActivity(didntUnderstandMessage);
+                    break;
+            }
+            return stepContext.next(null);
+        });
     }
 
+    /**
+     * Shows a warning if the requested From or To cities are recognized as entities but they are not in the Airport entity list.
+     * In some cases LUIS will recognize the From and To composite entities as a valid cities but the From and To Airport values
+     * will be empty if those entity values can't be mapped to a canonical item in the Airport.
+     * @param turnContext A {@link WaterfallStepContext}
+     * @param fromEntities An ObjectNode with the entities of From object
+     * @param toEntities An ObjectNode with the entities of To object
+     * @return A task
+     */
     private static CompletableFuture<Void> showWarningForUnsupportedCities(TurnContext turnContext, ObjectNode fromEntities, ObjectNode toEntities) {
         List<String> unsupportedCities = new ArrayList<String>();
 
-        if (StringUtils.isNotEmpty(fromEntities.get("From").toString()) && StringUtils.isEmpty(fromEntities.get("Airport").toString())) {
-            unsupportedCities.add(fromEntities.get("From").toString());
+        if (StringUtils.isNotBlank(fromEntities.get("from").toString()) && StringUtils.isBlank(fromEntities.get("airport").toString())) {
+            unsupportedCities.add(fromEntities.get("from").toString());
         }
 
-        if (StringUtils.isNotEmpty(toEntities.get("To").toString()) && StringUtils.isEmpty(toEntities.get("Airport").toString())) {
-            unsupportedCities.add(toEntities.get("To").toString());
+        if (StringUtils.isNotBlank(toEntities.get("to").toString()) && StringUtils.isBlank(toEntities.get("airport").toString())) {
+            unsupportedCities.add(toEntities.get("to").toString());
         }
 
         if (!unsupportedCities.isEmpty()) {
-            String messageText = String.format("Sorry but the following airports are not supported: %s", String.join(",", unsupportedCities));
+            String messageText = String.format("Sorry but the following airports are not supported: %s", String.join(", ", unsupportedCities));
             Activity message = MessageFactory.text(messageText, messageText, InputHints.IGNORING_INPUT);
             turnContext.sendActivity(message).thenApply(sendResult -> null);
         }
@@ -126,6 +154,12 @@ public class MainDialog extends ComponentDialog {
         return CompletableFuture.completedFuture(null);
     }
 
+    /**
+     * This is the final step in the main waterfall dialog.
+     * It wraps up the sample "book a flight" interaction with a simple confirmation.
+     * @param stepContext A {@link WaterfallStepContext}
+     * @return A {@link DialogTurnResult}
+     */
     private CompletableFuture<DialogTurnResult> finalStep(WaterfallStepContext stepContext) {
         // If the child dialog ("BookingDialog") was cancelled, the user failed to confirm or if the intent wasn't BookFlight
         // the Result here will be null.
@@ -135,9 +169,9 @@ public class MainDialog extends ComponentDialog {
             // If the call to the booking service was successful tell the user.
 
             BookingDetails result = (BookingDetails)stepContext.getResult();
-            TimexProperty timexProperty = new TimexProperty(result.getTravelDate());
-            String travelDateMsg = timexProperty.toNaturalLanguage(LocalDateTime.now());
-            String messageText = String.format("I have you booked to %1s from %2d on %3d", result.getDestination(), result.getOrigin(), travelDateMsg);
+            TimexProperty timeProperty = new TimexProperty(result.getTravelDate());
+            String travelDateMsg = timeProperty.toNaturalLanguage(LocalDateTime.now());
+            String messageText = String.format("I have you booked to %s from %s on %s", result.getDestination(), result.getOrigin(), travelDateMsg);
             Activity message = MessageFactory.text(messageText, messageText, InputHints.IGNORING_INPUT);
             stepContext.getContext().sendActivity(message).thenApply(sendResult -> null);
         }

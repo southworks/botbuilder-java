b1bf22563d52080833991d8fe65497529d253600
Federico Bernal <64086728+FedericoBernal@users.noreply.github.com>
2021-03-10 16:08:57 -0300

    Add activityLog class
    * Add activityLog class
    
    * Fix ActivityLog class
    
    * Delete unused import
    
    * Apply feedback
    
    * Apply feedback
    
    * Apply requested changes
    
    * Fix ActivityLog
    
    * Delete exception from the method's signature

---
+++ samples/25.message-reaction/src/main/java/com/microsoft/bot/sample/messagereaction/ActivityLog.java
@@ -0,0 +1,42 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.microsoft.bot.sample.messagereaction;
+
+import com.microsoft.bot.builder.Storage;
+import com.microsoft.bot.schema.Activity;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+
+public class ActivityLog {
+
+    private Storage storage;
+
+    public void ActivityLog(Storage withStorage) {
+        storage = withStorage;
+    }
+
+    public CompletableFuture<Void> append(String activityId, Activity activity) {
+        if (activityId == null) {
+            throw new IllegalArgumentException("activityId");
+        }
+
+        if (activity == null) {
+            throw new IllegalArgumentException("activity");
+        }
+
+        Map<String, Object> dictionary = new HashMap<String, Object>();
+        return storage.write((Map<String, Object>) dictionary.put(activityId, activity));
+    }
+
+    public CompletableFuture<Activity> find(String activityId) {
+        if (activityId == null) {
+            throw new IllegalArgumentException("activityId");
+        }
+
+        return storage.read(new String[]{activityId}).thenApply(activitiesResult -> {
+            return activitiesResult.size() >= 1 ? ((Activity) activitiesResult.get(activityId)) : null;
+        });
+    }
+}

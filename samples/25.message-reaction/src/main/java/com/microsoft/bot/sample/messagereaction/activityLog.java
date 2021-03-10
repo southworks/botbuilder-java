0b695efe40cbfc420e8eca5e25ef10cb671d5ef5
Federico Bernal <64086728+FedericoBernal@users.noreply.github.com>
2021-03-10 14:02:09 -0300

    Add activityLog class

---
+++ samples/25.message-reaction/src/main/java/com/microsoft/bot/sample/messagereaction/activityLog.java
@@ -0,0 +1,42 @@
+package com.microsoft.bot.sample.messagereaction;
+
+import com.microsoft.bot.builder.Storage;
+import com.microsoft.bot.schema.Activity;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+
+public class activityLog {
+
+    private Storage _storage;
+
+    public void activityLog(Storage storage) {
+        _storage = storage;
+    }
+
+    public CompletableFuture<Void> Append(String activityId, Activity activity) throws IllegalAccessException {
+        if (activityId == null) {
+            throw new IllegalAccessException("activityId");
+        }
+
+        if (activity == null) {
+            throw new IllegalArgumentException("activity");
+        }
+
+        Map<String, Object> dictionary = new HashMap<String, Object>();
+        return _storage.write((Map<String, Object>) dictionary.put(activityId, activity));
+    }
+
+    public CompletableFuture<Activity> Find(String activityId) {
+        if (activityId == null) {
+            throw new IllegalArgumentException("activityId");
+        }
+
+        _storage.read(new String[]{activityId}).thenCompose(activitiesResult -> {
+        return activitiesResult.size() >= 1 ? (Activity) activitiesResult.get(activityId) : null;
+        });
+    }
+}
+
+

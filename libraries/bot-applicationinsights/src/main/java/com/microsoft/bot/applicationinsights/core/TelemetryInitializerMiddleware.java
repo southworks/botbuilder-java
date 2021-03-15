1245181a83502a5b9dfd6c2df45a88166f0d40ca
MatR <matias.roldan@southworks.com>
2021-03-15 12:07:11 -0300

    Add TelemetryInitializerMiddleware class
    * Add TelemetryInitializerMiddleware class
    
    * Add PR requested changes

---
+++ libraries/bot-applicationinsights/src/main/java/com/microsoft/bot/applicationinsights/core/TelemetryInitializerMiddleware.java
@@ -0,0 +1,57 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for
+// license information.
+
+package com.microsoft.bot.applicationinsights.core;
+
+import com.microsoft.applicationinsights.core.dependencies.http.client.protocol.HttpClientContext;
+import com.microsoft.applicationinsights.core.dependencies.http.protocol.HttpContext;
+import com.microsoft.bot.builder.BotAssert;
+import com.microsoft.bot.builder.Middleware;
+import com.microsoft.bot.builder.NextDelegate;
+import com.microsoft.bot.builder.TelemetryLoggerMiddleware;
+import com.microsoft.bot.builder.TurnContext;
+import com.microsoft.bot.schema.Activity;
+
+import java.util.concurrent.CompletableFuture;
+
+public class TelemetryInitializerMiddleware implements Middleware {
+    
+    private HttpContext httpContext;
+    private final TelemetryLoggerMiddleware telemetryLoggerMiddleware;
+    private final Boolean logActivityTelemetry;
+
+    public TelemetryInitializerMiddleware(TelemetryLoggerMiddleware withTelemetryLoggerMiddleware, Boolean withLogActivityTelemetry) {
+        telemetryLoggerMiddleware = withTelemetryLoggerMiddleware;
+        logActivityTelemetry = withLogActivityTelemetry != null ? withLogActivityTelemetry : true;
+    }
+
+    public CompletableFuture<Void> onTurn(TurnContext context, NextDelegate next) {
+        BotAssert.contextNotNull(context);
+
+        if (context.getActivity() != null) {
+            Activity activity = context.getActivity();
+
+            if (this.httpContext == null) {
+                this.httpContext = HttpClientContext.create();
+            }
+
+            Object item = httpContext.getAttribute(TelemetryBotIdInitializer.BotActivityKey);
+
+            if (item != null) {
+                httpContext.removeAttribute(TelemetryBotIdInitializer.BotActivityKey);
+            }
+
+            httpContext.setAttribute(TelemetryBotIdInitializer.BotActivityKey, activity);
+        }
+
+        if (logActivityTelemetry) {
+            return telemetryLoggerMiddleware.onTurn(context, next);
+        } else {
+            return next.next();
+        }
+
+        return CompletableFuture.completedFuture(null);
+    }
+}
+

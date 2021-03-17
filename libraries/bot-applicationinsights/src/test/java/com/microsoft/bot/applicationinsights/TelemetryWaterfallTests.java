3480fa63416129cee59c4bdb5e31e69cfe9649ea
Victor <victor@grycuk.net>
2021-03-17 10:09:03 -0300

    Fix bug, add test

--- libraries/bot-applicationinsights/src/test/java/com/microsoft/bot/applicationinsights/TelemetryWaterfallTests.java
+++ libraries/bot-applicationinsights/src/test/java/com/microsoft/bot/applicationinsights/TelemetryWaterfallTests.java
@@ -77,9 +77,13 @@ public class TelemetryWaterfallTests {
         .startTest()
         .join();
 
+        // C#'s trackEvent method of BotTelemetryClient has nullable parameters,
+        // therefore it always calls the same method.
+        // On the other hand, Java's BotTelemetryClient overloads the trackEvent method,
+        // so instead of calling the same method, it calls a method with less parameters.
+        // In this particular test, WaterfallDialog's beginDialog calls the method with only two parameters
         verify(telemetryClient, times(4)).trackEvent(
             anyString(),
-            anyMap(),
             anyMap()
         );
     }
@@ -115,9 +119,13 @@ public class TelemetryWaterfallTests {
         .startTest()
         .join();
 
+        // C#'s trackEvent method of BotTelemetryClient has nullable parameters,
+        // therefore it always calls the same method.
+        // On the other hand, Java's BotTelemetryClient overloads the trackEvent method,
+        // so instead of calling the same method, it calls a method with less parameters.
+        // In this particular test, WaterfallDialog's beginDialog calls the method with only two parameters
         verify(telemetryClient, times(4)).trackEvent(
             anyString(),
-            anyMap(),
             anyMap()
         );
     }
@@ -138,10 +146,9 @@ public class TelemetryWaterfallTests {
     @Test
     public void ensureEndDialogCalled() {
         ConversationState convoState = new ConversationState(new MemoryStorage());
-        TestAdapter adapter = new TestAdapter(TestAdapter.createConversationReference("waterfallWithCallback", "User1", "Bot"))
+        TestAdapter adapter = new TestAdapter(TestAdapter.createConversationReference("ensureEndDialogCalled", "User1", "Bot"))
             .use(new AutoSaveStateMiddleware(convoState));
 
-        //BotTelemetryClient telemetryClient = Mockito.mock(BotTelemetryClient.class);
         StatePropertyAccessor<DialogState> dialogState = convoState.createProperty("dialogState");
         DialogSet dialogs = new DialogSet(dialogState);
         HashMap<String, Map<String, String>> saved_properties = new HashMap<>();
@@ -224,9 +231,115 @@ public class TelemetryWaterfallTests {
 
         // Verify naming on lambda's is "StepXofY"
         Assert.assertTrue(saved_properties.get("WaterfallStep_1").containsKey("StepName"));
-        Assert.assertEquals("Step1of3", saved_properties.get("WaterfallComplete_4").get("StepName"));
-        Assert.assertTrue(saved_properties.get("WaterfallStep_1").containsKey("InstanceId")); // This assert is repeated
+        Assert.assertEquals("Step1of3", saved_properties.get("WaterfallStep_1").get("StepName"));
         Assert.assertTrue(waterfallDialog.getEndDialogCalled());
+    }
+
+    @Test
+    public void ensureCancelDialogCalled() {
+        ConversationState convoState = new ConversationState(new MemoryStorage());
+        TestAdapter adapter = new TestAdapter(TestAdapter.createConversationReference("ensureCancelDialogCalled", "User1", "Bot"))
+            .use(new AutoSaveStateMiddleware(convoState));
+
+        StatePropertyAccessor<DialogState> dialogState = convoState.createProperty("dialogState");
+        DialogSet dialogs = new DialogSet(dialogState);
+        HashMap<String, Map<String, String>> saved_properties = new HashMap<>();
+        final Integer[] counter = {0};
+
+        // Set up the client to save all logged property names and associated properties (in "saved_properties").
+        BotTelemetryClient telemetryClient = new BotTelemetryClient() {
+            @Override
+            public void trackAvailability(String name, OffsetDateTime timeStamp, Duration duration, String runLocation, boolean success, String message, Map<String, String> properties, Map<String, Double> metrics) {
+
+            }
+
+            @Override
+            public void trackDependency(String dependencyTypeName, String target, String dependencyName, String data, OffsetDateTime startTime, Duration duration, String resultCode, boolean success) {
+
+            }
+
+            @Override
+            public void trackEvent(String eventName, Map<String, String> properties, Map<String, Double> metrics) {
+                StringBuilder sb = new StringBuilder(eventName).append("_").append(counter[0]++);
+                saved_properties.put(sb.toString(), properties);
+            }
+
+            @Override
+            public void trackException(Exception exception, Map<String, String> properties, Map<String, Double> metrics) {
+
+            }
+
+            @Override
+            public void trackTrace(String message, Severity severityLevel, Map<String, String> properties) {
+
+            }
+
+            @Override
+            public void trackDialogView(String dialogName, Map<String, String> properties, Map<String, Double> metrics) {
+
+            }
+
+            @Override
+            public void flush() {
+
+            }
+        };
+
+        List<WaterfallStep> steps = new ArrayList<>();
+        steps.add(step -> {
+            step.getContext().sendActivity("step1");
+            return CompletableFuture.completedFuture(Dialog.END_OF_TURN);
+        });
+        steps.add(step -> {
+            step.getContext().sendActivity("step2");
+            return CompletableFuture.completedFuture(Dialog.END_OF_TURN);
+        });
+        steps.add(step -> {
+            step.cancelAllDialogs();
+            return CompletableFuture.completedFuture(Dialog.END_OF_TURN);
+        });
+
+        MyWaterfallDialog waterfallDialog = new MyWaterfallDialog("test", steps);
+
+        dialogs.add(waterfallDialog);
+        dialogs.setTelemetryClient(telemetryClient);
+
+        new TestFlow(adapter, turnContext -> {
+            DialogContext dc = dialogs.createContext(turnContext).join();
+            dc.continueDialog().join();
+            if (!turnContext.getResponded()) {
+                dc.beginDialog("test", null).join();
+            }
+            return CompletableFuture.completedFuture(null);
+        })
+            .send("hello")
+            .assertReply("step1")
+            .send("hello")
+            .assertReply("step2")
+            .send("hello")
+            .assertReply("step1")
+            .startTest()
+            .join();
+
+        Assert.assertEquals((Integer)7, counter[0]);
+
+        // Verify:
+        // Event name is "WaterfallCancel"
+        // Event occurs on the 4th event logged
+        // Event contains DialogId
+        // Event DialogId is set correctly.
+        Assert.assertTrue(saved_properties.get("WaterfallStart_0").containsKey("DialogId"));
+        Assert.assertTrue(saved_properties.get("WaterfallStart_0").containsKey("InstanceId"));
+        Assert.assertTrue(saved_properties.get("WaterfallCancel_4").containsKey("DialogId"));
+        Assert.assertEquals("test", saved_properties.get("WaterfallCancel_4").get("DialogId"));
+        Assert.assertTrue(saved_properties.get("WaterfallCancel_4").containsKey("StepName"));
+        Assert.assertTrue(saved_properties.get("WaterfallCancel_4").containsKey("InstanceId"));
+
+        // Event contains "StepName"
+        // Event naming on lambda's is "StepXofY"
+        Assert.assertEquals("Step3of3", saved_properties.get("WaterfallCancel_4").get("StepName"));
+        Assert.assertTrue(waterfallDialog.getCancelDialogCalled());
+        Assert.assertFalse(waterfallDialog.getEndDialogCalled());
     }
 
     private static List<WaterfallStep> newWaterfall() {
@@ -263,8 +376,11 @@ public class TelemetryWaterfallTests {
 
         @Override
         public CompletableFuture<Void> endDialog(TurnContext turnContext, DialogInstance instance, DialogReason reason) {
-            endDialogCalled = reason == DialogReason.END_CALLED;
-            cancelDialogCalled = reason == DialogReason.CANCEL_CALLED;
+            if (reason == DialogReason.END_CALLED) {
+                endDialogCalled = true;
+            } else if (reason == DialogReason.CANCEL_CALLED) {
+                cancelDialogCalled = true;
+            }
 
             return super.endDialog(turnContext, instance, reason);
         }

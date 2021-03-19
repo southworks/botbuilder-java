925a171bf161628c945942b0a90e9aed46e48439
Franco Alvarez <franco.alvarez@southworks.com>
2021-03-19 15:06:50 -0300

    Fix the remaining broken test

--- libraries/bot-applicationinsights/src/test/java/com/microsoft/bot/applicationinsights/TelemetryInitializerTests.java
+++ libraries/bot-applicationinsights/src/test/java/com/microsoft/bot/applicationinsights/TelemetryInitializerTests.java
@@ -6,11 +6,16 @@ package com.microsoft.bot.applicationinsights;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mockito;
 
 import com.microsoft.applicationinsights.TelemetryClient;
@@ -21,8 +26,16 @@ import com.microsoft.bot.builder.adapters.TestAdapter;
 import com.microsoft.bot.builder.adapters.TestFlow;
 import com.microsoft.bot.schema.Activity;
 import com.microsoft.bot.schema.ActivityTypes;
+import org.mockito.junit.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class TelemetryInitializerTests {
+
+    @Captor
+    ArgumentCaptor<String> eventNameCaptor;
+
+    @Captor
+    ArgumentCaptor<Map<String, String>> propertiesCaptor;
 
 	@Test
 	public void telemetryInitializerMiddlewareLogActivitiesEnabled() {
@@ -32,7 +45,7 @@ public class TelemetryInitializerTests {
 		TelemetryLoggerMiddleware telemetryLoggerMiddleware = new TelemetryLoggerMiddleware(mockTelemetryClient, false);
 
 		TestAdapter testAdapter = new TestAdapter()
-				.use(new TelemetryInitializerMiddleware(telemetryLoggerMiddleware, true));
+            .use(new TelemetryInitializerMiddleware(telemetryLoggerMiddleware, true));
 
 		// Act
 		// Default case logging Send/Receive Activities
@@ -62,18 +75,25 @@ public class TelemetryInitializerTests {
 		.startTest().join();
 
 		// Verify
-		verify(mockTelemetryClient, times(6));
+        verify(mockTelemetryClient, times(6)).trackEvent(
+            eventNameCaptor.capture(),
+            propertiesCaptor.capture()
+            );
+
+        List<String> eventNames = eventNameCaptor.getAllValues();
+        Assert.assertEquals(6, eventNames.size());
+
 	}
 
 	@Test
 	public void telemetryInitializerMiddlewareNotLogActivitiesDisabled() {
 
 		// Arrange
-		BotTelemetryClient mockTelemetryClient = Mockito.mock(BotTelemetryClient.class);
+        BotTelemetryClient mockTelemetryClient = Mockito.mock(BotTelemetryClient.class);
 		TelemetryLoggerMiddleware telemetryLoggerMiddleware = new TelemetryLoggerMiddleware(mockTelemetryClient, false);
 
 		TestAdapter testAdapter = new TestAdapter()
-				.use(new TelemetryInitializerMiddleware(telemetryLoggerMiddleware, false));
+            .use(new TelemetryInitializerMiddleware(telemetryLoggerMiddleware, false));
 
 		// Act
 		// Default case logging Send/Receive Activities
@@ -103,6 +123,11 @@ public class TelemetryInitializerTests {
 		.startTest().join();
 
 		// Verify
-		verify(mockTelemetryClient, times(0));
+        verify(mockTelemetryClient, times(0)).trackEvent(
+            eventNameCaptor.capture(),
+            propertiesCaptor.capture()
+        );
+        List<String> eventNames = eventNameCaptor.getAllValues();
+        Assert.assertEquals(0, eventNames.size());
 	}
 }

2813e4d47a302010e0aa451d249d417ff5081a1c
Franco Alvarez <51216149+fran893@users.noreply.github.com>
2021-03-30 19:55:38 -0300

    Migrate BotServices interface
    * Migrate BotServices interface
    
    * Remove empty line
    
    Co-authored-by: Martin Battaglino <martinbatta32@gmail.com>

---
+++ samples/14.nlp-with-dispatch/src/main/java/com/microsoft/bot/sample/nlp/with/dispatch/BotServices.java
@@ -0,0 +1,25 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.microsoft.bot.sample.nlp.with.dispatch;
+
+import com.microsoft.bot.ai.luis.LuisRecognizer;
+import com.microsoft.bot.ai.qna.QnAMaker;
+
+/**
+ * Interface which contains the LuisRecognizer dispatch and the QnAMaker application.
+ */
+public interface BotServices {
+
+    /**
+     * Gets the {@link LuisRecognizer} dispatch
+     * @return The LuisRecognizer dispatch
+     */
+    LuisRecognizer getDispatch();
+
+    /**
+     * Gets the {@link QnAMaker} application
+     * @return The QnAMaker application
+     */
+    QnAMaker getSampleQnA();
+}

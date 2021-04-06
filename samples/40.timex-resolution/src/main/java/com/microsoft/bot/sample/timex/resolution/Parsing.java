31239a9cc7591c4dee25411acc068c67ccb60fde
Federico Bernal <64086728+FedericoBernal@users.noreply.github.com>
2021-04-06 09:27:25 -0300

    Apply feedback

--- samples/40.timex-resolution/src/main/java/com/microsoft/bot/sample/timex/resolution/Parsing.java
+++ samples/40.timex-resolution/src/main/java/com/microsoft/bot/sample/timex/resolution/Parsing.java
@@ -6,11 +6,20 @@ package com.microsoft.bot.sample.timex.resolution;
 import com.microsoft.recognizers.datatypes.timex.expression.Constants.TimexTypes;
 import com.microsoft.recognizers.datatypes.timex.expression.TimexProperty;
 
+   /** The TimexProperty class takes a TIMEX expression as a string argument in its constructor.
+    * This pulls all the component parts of the expression into properties on this object. You can
+    * then manipulate the TIMEX expression via those properties.
+    * The "Types" property infers a datetimeV2 type from the underlying set of properties.
+    * If you take a TIMEX with date components and add time components you add the
+    * inferred type datetime (its still a date).
+    * Logic can be written against the inferred type, perhaps to have the bot ask the user for disambiguation.
+    */
+
 public class Parsing
 {
-    private static void Describe(TimexProperty t)
+    private static void describe(TimexProperty t)
     {
-        System.out.println("{t.TimexValue}");
+        System.out.println("{t.getTimexValue}");
 
         if (t.getTypes().contains(TimexTypes.DATE))
         {
@@ -32,11 +41,11 @@ public class Parsing
         System.out.println();
     }
 
-    public static void Examples()
+    public static void examples()
     {
-        Describe(new TimexProperty("2017-05-29"));
-        Describe(new TimexProperty("XXXX-WXX-6"));
-        Describe(new TimexProperty("XXXX-WXX-6T16"));
-        Describe(new TimexProperty("T12"));
+        describe(new TimexProperty("2017-05-29"));
+        describe(new TimexProperty("XXXX-WXX-6"));
+        describe(new TimexProperty("XXXX-WXX-6T16"));
+        describe(new TimexProperty("T12"));
     }
 }

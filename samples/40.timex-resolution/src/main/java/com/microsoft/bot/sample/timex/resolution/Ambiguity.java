92fe13e49e585bf7a495714e2adbb703b920d2a9
Martin Battaglino <martinbatta32@gmail.com>
2021-04-06 15:23:22 -0300

    Apply remaining nit changes

--- samples/40.timex-resolution/src/main/java/com/microsoft/bot/sample/timex/resolution/Ambiguity.java
+++ samples/40.timex-resolution/src/main/java/com/microsoft/bot/sample/timex/resolution/Ambiguity.java
@@ -11,101 +11,96 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
-    /**
-     * TIMEX expressions are designed to represent ambiguous rather than definite dates.
-     * For example:
-     * "Monday" could be any Monday ever.
-     * "May 5th" could be any one of the possible May 5th in the past or the future.
-     * TIMEX does not represent ambiguous times. So if the natural language mentioned 4 o'clock
-     * it could be either 4AM or 4PM. For that the recognizer (and by extension LUIS) would return two TIMEX expressions.
-     * A TIMEX expression can include a date and time parts. So ambiguity of date can be combined with multiple results.
-     * Code that deals with TIMEX expressions is frequently dealing with sets of TIMEX expressions.
-     */
-public class Ambiguity
-{
-    public static void dateAmbiguity()
-    {
+/**
+ * TIMEX expressions are designed to represent ambiguous rather than definite dates.
+ * For example:
+ * "Monday" could be any Monday ever.
+ * "May 5th" could be any one of the possible May 5th in the past or the future.
+ * TIMEX does not represent ambiguous times. So if the natural language mentioned 4 o'clock
+ * it could be either 4AM or 4PM. For that the recognizer (and by extension LUIS) would return two TIMEX expressions.
+ * A TIMEX expression can include a date and time parts. So ambiguity of date can be combined with multiple results.
+ * Code that deals with TIMEX expressions is frequently dealing with sets of TIMEX expressions.
+ */
+public class Ambiguity {
 
+    public static void dateAmbiguity() {
         // Run the recognizer.
-        List<ModelResult> results = DateTimeRecognizer.recognizeDateTime("Either Saturday or Sunday would work.", Culture.English);
+        List<ModelResult> results = DateTimeRecognizer.recognizeDateTime(
+            "Either Saturday or Sunday would work.",
+            Culture.English);
 
         // We should find two results in this example.
-        for (ModelResult result : results)
-        {
+        for (ModelResult result : results) {
             // The resolution includes two example values: going backwards and forwards from NOW in the calendar.
             HashSet<String> distinctTimexExpressions = new HashSet<String>();
-            List<Map<String, String>> values = (List<Map<String, String>>)result.resolution.get("values");
-            for (Map<String, String> value : values)
-            {
+            List<Map<String, String>> values = (List<Map<String, String>>) result.resolution.get("values");
+            for (Map<String, String> value : values) {
                 // Each result includes a TIMEX expression that captures the inherent date but not time ambiguity.
                 // We are interested in the distinct set of TIMEX expressions.
-                if (values.contains("timex"))
-                {
-                    String timex = value.get("timex");
+                String timex = value.get("timex");
+                if (timex != null) {
                     distinctTimexExpressions.add(timex);
                 }
+
                 // There is also either a "value" property on each value or "start" and "end".
                 // If you use ToString() on a TimeProperty object you will get same "value".
             }
 
             // The TIMEX expression captures date ambiguity so there will be a single distinct expression for each result.
-            result.text.concat((String.join(",", distinctTimexExpressions)));
+            String output = String.format("%s ( %s )", result.text, String.join(",", distinctTimexExpressions));
+            System.out.println(output);
 
             // The result also includes a reference to the original string - but note the start and end index are both inclusive.
-            }
+        }
     }
 
-    public static void timeAmbiguity()
-    {
+    public static void timeAmbiguity() {
         // Run the recognizer.
-        List<ModelResult> results = DateTimeRecognizer.recognizeDateTime("We would like to arrive at 4 o'clock or 5 o'clock.", Culture.English);
+        List<ModelResult> results = DateTimeRecognizer.recognizeDateTime(
+            "We would like to arrive at 4 o'clock or 5 o'clock.",
+            Culture.English);
 
         // We should find two results in this example.
-        for (ModelResult result : results)
-        {
+        for (ModelResult result : results) {
             // The resolution includes two example values: one for AM and one for PM.
             HashSet<String> distinctTimexExpressions = new HashSet<String>();
-            List<Map<String, String>> values = (List<Map<String, String>>)result.resolution.get("values");
-            for (Map<String, String> value : values)
-            {
+            List<Map<String, String>> values = (List<Map<String, String>>) result.resolution.get("values");
+            for (Map<String, String> value : values) {
                 // Each result includes a TIMEX expression that captures the inherent date but not time ambiguity.
                 // We are interested in the distinct set of TIMEX expressions.
-                if (values.contains("timex"))
-                {
-                    String timex = value.get("timex");
+                String timex = value.get("timex");
+                if (timex != null) {
                     distinctTimexExpressions.add(timex);
                 }
             }
 
             // TIMEX expressions don't capture time ambiguity so there will be two distinct expressions for each result.
-            result.text.concat((String.join(",", distinctTimexExpressions)));
+            String output = String.format("%s ( %s )", result.text, String.join(",", distinctTimexExpressions));
+            System.out.println(output);
         }
     }
 
-    public static void dateTimeAmbiguity()
-    {
+    public static void dateTimeAmbiguity() {
         // Run the recognizer.
         List<ModelResult> results = DateTimeRecognizer.recognizeDateTime("It will be ready Wednesday at 5 o'clock.", Culture.English);
 
         // We should find a single result in this example.
-        for (ModelResult result : results)
-        {
+        for (ModelResult result : results) {
             // The resolution includes four example values: backwards and forward in the calendar and then AM and PM.
             HashSet<String> distinctTimexExpressions = new HashSet<String>();
-            List<Map<String, String>> values = (List<Map<String, String>>)result.resolution.get("values");
-            for (Map<String, String> value : values)
-            {
+            List<Map<String, String>> values = (List<Map<String, String>>) result.resolution.get("values");
+            for (Map<String, String> value : values) {
                 // Each result includes a TIMEX expression that captures the inherent date but not time ambiguity.
                 // We are interested in the distinct set of TIMEX expressions.
-                if (values.contains("timex"))
-                {
-                    String timex = value.get("timex");
+                String timex = value.get("timex");
+                if (timex != null) {
                     distinctTimexExpressions.add(timex);
                 }
             }
 
             // TIMEX expressions don't capture time ambiguity so there will be two distinct expressions for each result.
-            result.text.concat((String.join(",", distinctTimexExpressions)));
+            String output = String.format("%s ( %s )", result.text, String.join(",", distinctTimexExpressions));
+            System.out.println(output);
         }
     }
 }

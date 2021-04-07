77ce0cca800de9a716ae1d0012c0ae18a3522b4b
matiasroldan6 <matias.roldan@southworks.com>
2021-04-07 15:54:46 -0300

    Add Application.java checkstyle fixes

--- samples/40.timex-resolution/src/main/java/com/microsoft/bot/sample/timex/resolution/Application.java
+++ samples/40.timex-resolution/src/main/java/com/microsoft/bot/sample/timex/resolution/Application.java
@@ -3,7 +3,17 @@
 
 package com.microsoft.bot.sample.timex.resolution;
 
-public class Application {
+/**
+ * This is the main program class.
+ */
+public final class Application {
+    private Application() {
+    }
+    
+    /**
+     * This is the entry point method.
+     * @param args String array to capture any command line parameters.
+     */
     static void main(String[] args) {
         // Creating TIMEX expressions from natural language using the Recognizer package.
         Ambiguity.dateAmbiguity();

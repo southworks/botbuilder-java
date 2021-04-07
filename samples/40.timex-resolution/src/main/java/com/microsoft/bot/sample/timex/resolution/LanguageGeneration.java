78cefe73890ea2ef91d43639f3a3fe1f7d8c08f9
Federico Bernal <64086728+FedericoBernal@users.noreply.github.com>
2021-04-07 11:47:09 -0300

    Apply feedback

--- samples/40.timex-resolution/src/main/java/com/microsoft/bot/sample/timex/resolution/LanguageGeneration.java
+++ samples/40.timex-resolution/src/main/java/com/microsoft/bot/sample/timex/resolution/LanguageGeneration.java
@@ -12,18 +12,15 @@ import java.time.LocalDateTime;
  * As an experiment try feeding the result of language generation back into a recognizer.
  * You should get back the same TIMEX expression in the result.
  */
-public class LanguageGeneration
-{
-    private static void describe(TimexProperty t)
-    {
+public class LanguageGeneration{
+    private static void describe(TimexProperty t){
         // Note natural language is often relative, for example the sentence "Yesterday all my troubles seemed so far away."
         // Having your bot say something like "next Wednesday" in a response can make it sound more natural.
         LocalDateTime referenceDate = LocalDateTime.now();
         System.out.println(t.getTimexValue() + t.toNaturalLanguage(referenceDate));
     }
 
-    public static void examples()
-    {
+    public static void examples(){
         describe(new TimexProperty("2019-05-29"));
         describe(new TimexProperty("XXXX-WXX-6"));
         describe(new TimexProperty("XXXX-WXX-6T16"));

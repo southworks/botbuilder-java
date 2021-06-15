db410ff39194e0d113ddddb8ad544d6a884ba13e
Martin Battaglino <martinbatta32@gmail.com>
2021-06-15 11:58:43 -0300

    Update BotFrameworkAuthenticationFactory

--- libraries/bot-connector/src/main/java/com/microsoft/bot/connector/authentication/BotFrameworkAuthenticationFactory.java
+++ libraries/bot-connector/src/main/java/com/microsoft/bot/connector/authentication/BotFrameworkAuthenticationFactory.java
@@ -3,6 +3,7 @@
 
 package com.microsoft.bot.connector.authentication;
 
+import com.microsoft.bot.schema.CallerIdConstants;
 import org.apache.commons.lang3.StringUtils;
 
 /**
@@ -50,7 +51,6 @@ public final class BotFrameworkAuthenticationFactory {
             StringUtils.isNotBlank(toBotFromEmulatorOpenIdMetadataUrl) ||
             StringUtils.isNotBlank(callerId)) {
             // if we have any of the 'parameterized' properties defined we'll assume this is the parameterized code
-
             return new ParameterizedBotFrameworkAuthentication(
                 validateAuthority,
                 toChannelFromBotLoginUrl,
@@ -66,15 +66,35 @@ public final class BotFrameworkAuthenticationFactory {
         } else {
             // else apply the built in default behavior, which is either the public cloud or the gov cloud
             // depending on whether we have a channelService value present
-
             if (StringUtils.isBlank(channelService)) {
-                return new PublicCloudBotFrameworkAuthentication(credentialFactory, authConfiguration);
+                return new ParameterizedBotFrameworkAuthentication(
+                    true,
+                    AuthenticationConstants.TO_CHANNEL_FROM_BOT_LOGIN_URL,
+                    AuthenticationConstants.TO_CHANNEL_FROM_BOT_OAUTH_SCOPE,
+                    AuthenticationConstants.TO_BOT_FROM_CHANNEL_TOKEN_ISSUER,
+                    AuthenticationConstants.OAUTH_URL,
+                    AuthenticationConstants.TO_BOT_FROM_CHANNEL_OPENID_METADATA_URL,
+                    AuthenticationConstants.TO_BOT_FROM_EMULATOR_OPENID_METADATA_URL,
+                    CallerIdConstants.PUBLIC_AZURE_CHANNEL,
+                    credentialFactory,
+                    authConfiguration
+                );
             } else if (channelService == GovernmentAuthenticationConstants.CHANNELSERVICE) {
-                return new GovernmentCloudBotFrameworkAuthentication(credentialFactory, authConfiguration);
+                return new ParameterizedBotFrameworkAuthentication(
+                    true,
+                    GovernmentAuthenticationConstants.TO_CHANNEL_FROM_BOT_LOGIN_URL,
+                    GovernmentAuthenticationConstants.TO_CHANNEL_FROM_BOT_OAUTH_SCOPE,
+                    GovernmentAuthenticationConstants.TO_BOT_FROM_CHANNEL_TOKEN_ISSUER,
+                    GovernmentAuthenticationConstants.OAUTH_URL_GOV,
+                    GovernmentAuthenticationConstants.TO_BOT_FROM_CHANNEL_OPENID_METADATA_URL,
+                    GovernmentAuthenticationConstants.TO_BOT_FROM_EMULATOR_OPENID_METADATA_URL,
+                    CallerIdConstants.US_GOV_CHANNEL,
+                    credentialFactory,
+                    authConfiguration
+                );
             } else {
                 // The ChannelService value is used an indicator of which built in set of constants to use.
                 // If it is not recognized, a full configuration is expected.
-
                 throw new IllegalArgumentException("The provided ChannelService value is not supported.");
             }
         }

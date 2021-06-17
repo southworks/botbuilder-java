02ed583659f5f7f878664f612c3fdb0d454badc2
Martin Battaglino <martinbatta32@gmail.com>
2021-06-17 10:57:34 -0300

    Minor changes in DelegatingCredentialProvider

--- libraries/bot-connector/src/main/java/com/microsoft/bot/connector/authentication/DelegatingCredentialProvider.java
+++ libraries/bot-connector/src/main/java/com/microsoft/bot/connector/authentication/DelegatingCredentialProvider.java
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.microsoft.bot.connector.authentication;
 
 import org.apache.commons.lang3.NotImplementedException;
@@ -7,19 +10,19 @@ import java.util.concurrent.CompletableFuture;
  * This is just an internal class to allow pre-existing implementation of the request validation to be used with
  * a IServiceClientCredentialFactory.
  */
-public class DelegatingCredentialProvider implements ICredentialProvider {
+public class DelegatingCredentialProvider implements CredentialProvider {
     private ServiceClientCredentialsFactory credentialsFactory;
 
     public DelegatingCredentialProvider(ServiceClientCredentialsFactory withCredentialsFactory) {
         if (withCredentialsFactory == null) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("withCredentialsFactory cannot be null");
         }
 
         credentialsFactory = withCredentialsFactory;
     }
 
     public CompletableFuture<String> getAppPassword(String appId) {
-        throw new NotImplementedException("");
+        throw new NotImplementedException("getAppPassword is not implemented");
     }
 
     public CompletableFuture<Boolean> isAuthenticationDisabled() {

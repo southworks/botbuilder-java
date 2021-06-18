f99489582864bbf09415303d6c12fe96c329e66a
Martin Battaglino <martinbatta32@gmail.com>
2021-06-18 12:20:04 -0300

    Fix javadocs

--- libraries/bot-connector/src/main/java/com/microsoft/bot/connector/authentication/ConnectorFactoryImpl.java
+++ libraries/bot-connector/src/main/java/com/microsoft/bot/connector/authentication/ConnectorFactoryImpl.java
@@ -9,32 +9,35 @@ import java.util.concurrent.CompletableFuture;
 
 public class ConnectorFactoryImpl extends ConnectorFactory {
 
-    private String appId;
-    private String toChannelFromBotOAuthScope;
-    private String loginEndpoint;
-    private Boolean validateAuthority;
-    private ServiceClientCredentialsFactory credentialFactory;
+    private final String appId;
+    private final String toChannelFromBotOAuthScope;
+    private final String loginEndpoint;
+    private final Boolean validateAuthority;
+    private final ServiceClientCredentialsFactory credentialFactory;
 
     public ConnectorFactoryImpl(
-                                String withAppId,
-                                String withToChannelFromBotOAuthScope,
-                                String withLoginEndpoint,
-                                Boolean withValidateAuthority,
-                                ServiceClientCredentialsFactory withCredentialFactory) {
-        appId = withAppId;
-        toChannelFromBotOAuthScope = withToChannelFromBotOAuthScope;
-        loginEndpoint = withLoginEndpoint;
-        validateAuthority = withValidateAuthority;
-        credentialFactory = withCredentialFactory;
+        String withAppId,
+        String withToChannelFromBotOAuthScope,
+        String withLoginEndpoint,
+        Boolean withValidateAuthority,
+        ServiceClientCredentialsFactory withCredentialFactory) {
+        this.appId = withAppId;
+        this.toChannelFromBotOAuthScope = withToChannelFromBotOAuthScope;
+        this.loginEndpoint = withLoginEndpoint;
+        this.validateAuthority = withValidateAuthority;
+        this.credentialFactory = withCredentialFactory;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public CompletableFuture<ConnectorClient> create(String serviceUrl, String audience) {
-        // Use the credentials factory to create credentails specific to this particular cloud environment.
+        // Use the credentials factory to create credentials specific to this particular cloud environment.
         return credentialFactory.createCredentials(appId,
-                                            audience != null? audience : toChannelFromBotOAuthScope,
-                                            loginEndpoint,
-                                            validateAuthority).thenCompose(credentials -> {
+            audience != null? audience : toChannelFromBotOAuthScope,
+            loginEndpoint,
+            validateAuthority).thenCompose(credentials -> {
             // A new connector client for making calls against this serviceUrl using credentials
             // derived from the current appId and the specified audience.
             return CompletableFuture.completedFuture(new RestConnectorClient(serviceUrl, credentials));
